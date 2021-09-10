5330774cc2476e6e32df596775e290daf28ec78a
Papai Sarkar <iampapaisarkar@gmail.com>
2021-09-10 23:29:49 +0530

    hospital pharmacy dashboard template setup done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 @include('layouts.navbars.breadcrumb', ['page' => 'Dashboard', 'route' => 'dashboard'])
 <div class="row">
-    <div class="col-lg-6 col-md-12">
+    <div class="col-lg-12 col-md-12">
         <!-- CARD ICON-->
         <!-- Super admin cards start -->
         @can('isAdmin')
@@ -122,64 +122,36 @@
         <!-- Registration licencing cards end  -->
         
          <!-- Vendor cards start  -->
-        @can('isVendor')
+        @can('isHPharmacy')
         <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-6">
-                <div class="card card-icon mb-4">
-                    <div class="card-body text-center"><i class="i-Diploma"></i>
-                        <p class="text-muted mt-2 mb-2">{{$data['type']}}</p>
-                        <p class="text-primary text-20 line-height-1 m-0">{{$data['status']}}</p>
+            <div class="col-lg-8 col-md-8 col-sm-8">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6">
+                        <div class="card card-icon mb-4">
+                            <div class="card-body text-center"><i class="i-Diploma-2"></i>
+                                <p class="text-muted mt-2 mb-2">REGISTRATION</p>
+                                <p class="text-primary text-20 line-height-1 m-0">DOCS. REVIEW PENDING</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6">
+                        <div class="card card-icon mb-4">
+                            <div class="card-body text-center"><i class="i-Diploma-2"></i>
+                                <p class="text-muted mt-2 mb-2">REGISTRATION</p>
+                                <p class="text-primary text-20 line-height-1 m-0">DOCS. REVIEW PENDING</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <!-- <div class="col-lg-6 col-md-6 col-sm-6">
-                <div class="card card-icon mb-4">
-                    <div class="card-body text-center"><i class="i-Diploma"></i>
-                        <p class="text-muted mt-2 mb-2">METPT</p>
-                        <p class="text-primary text-20 line-height-1 m-0">DOCS. QUERIED</p>
+            <div class="col-lg-4 col-md-4 col-sm-4">
+                <div class="card mb-4">
+                    <div class="card-body p-0">
+                        <h5 class="card-title m-0 p-3">Sales</h5>
+                        <div id="echart4" style="height: 300px; -webkit-tap-highlight-color: transparent; user-select: none; position: relative;" _echarts_instance_="ec_1631296578561"><div style="position: relative; overflow: hidden; width: 310px; height: 300px; padding: 0px; margin: 0px; border-width: 0px; cursor: default;"><canvas data-zr-dom-id="zr_0" width="310" height="300" style="position: absolute; left: 0px; top: 0px; width: 310px; height: 300px; user-select: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); padding: 0px; margin: 0px; border-width: 0px;"></canvas></div><div></div></div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-6 col-md-6 col-sm-6">
-                <div class="card card-icon mb-4">
-                    <div class="card-body text-center"><i class="i-Diploma"></i>
-                        <p class="text-muted mt-2 mb-2">METPT</p>
-                        <p class="text-primary text-20 line-height-1 m-0">APPROVED</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-6">
-                <div class="card card-icon mb-4">
-                    <div class="card-body text-center"><i class="i-Diploma"></i>
-                        <p class="text-muted mt-2 mb-2">METPT</p>
-                        <p class="text-primary text-20 line-height-1 m-0">EXAM CARD</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-6">
-                <div class="card card-icon mb-4">
-                    <div class="card-body text-center"><i class="i-Diploma"></i>
-                        <p class="text-muted mt-2 mb-2">METPT</p>
-                        <p class="text-primary text-20 line-height-1 m-0">PASSED</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-6">
-                <div class="card card-icon mb-4">
-                    <div class="card-body text-center"><i class="i-Diploma"></i>
-                        <p class="text-muted mt-2 mb-2">METPT</p>
-                        <p class="text-primary text-20 line-height-1 m-0">FAILED</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-6">
-                <div class="card card-icon mb-4">
-                    <div class="card-body text-center"><i class="i-Diploma-2"></i>
-                        <p class="text-muted mt-2 mb-2">Tiered PPMV Registration</p>
-                        <p class="text-primary text-20 line-height-1 m-0">PENDING</p>
-                    </div>
-                </div>
-            </div> -->
         </div>
         @endcan
         <!-- Vendor cards end  -->
