eeaf12d134125d53781800d65c691312b046640a
Papai Sarkar <iampapaisarkar@gmail.com>
2021-09-12 14:25:06 +0530

    hospital pharmacy preview component done

---
+++ app/View/Components/HospitalPharmacyPreview.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+use App\Models\Registration;
+use App\Models\HospitalRegistration;
+
+class HospitalPharmacyPreview extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public $registration_id, $user_id;
+    public function __construct($registrationID, $userID)
+    {
+        $this->registration_id = $registrationID;
+        $this->user_id = $userID;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        $registration = Registration::where(['payment' => true, 'id' => $request['registration_id'], 'user_id' => $request['user_id'], 'type' => 'hospital_pharmacy'])
+        ->with('hospital_pharmacy', 'user')
+        ->first();
+
+        return view('components.hospital-pharmacy-preview', compact('registration'));
+    }
+}
--- resources/views/stateoffice/documents/hospital-show.blade.php
+++ resources/views/stateoffice/documents/hospital-show.blade.php
@@ -6,102 +6,11 @@
         <div class="col-lg-12 col-md-12">
             <div class="card-body">
                 <h5>Hospital Pharmacy Registration - Documents Verification</h5>
-                <div class="form-row">
-                    <div class="form-group col-md-3">
-                        <label for="inputEmail1" class="ul-form__label"><strong>Hospital Name:</strong></label>
-                        <div>{{$registration->user->hospital_name}}</div>
-                    </div>
-                    <div class="form-group col-md-3">
-                        <label for="inputEmail3" class="ul-form__label"><strong>Hospital Address:</strong></label>
-                        <div>{{$registration->user->hospital_address}}</div>
-                        
-                    </div>
-                    <div class="form-group col-md-3">
-                        <label for="inputEmail3" class="ul-form__label"><strong>State:</strong></label>
-                        <div>{{$registration->user->user_state->name}}</div>
-                        
-                    </div>
-                    <div class="form-group col-md-3">
-                        <label for="inputEmail3" class="ul-form__label"><strong>LGA:</strong> </label>
-                        <div>{{$registration->user->user_lga->name}}</div>
-                    </div>
-                </div>
-                    
-                    <div class="custom-separator"></div>
-                    <h4>Bed Capacity</h4>
-                    <div class="table-responsive">
-                        <table id="" class="display table table-striped table-bordered" style="width:100%">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Bed Capacity</th>
-                                    <th scope="col">Registration Fee</th>
-                                    <th scope="col">Inspection Fee</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @php
-                                    $bed = [];
-                                    foreach ( config('custom.beds') as $beds ) {
-                                    if ( $beds['id'] == $registration->hospital_pharmacy->bed_capacity ) {
-                                        $bed = $beds;
-                                    }
-                                }
-                                @endphp
-                                <tr>
-                                    <td>{{$bed['bed_capacity']}}</td>
-                                    <td>NGN {{number_format($bed['registration_fee'])}}</td>
-                                    <td>NGN {{number_format($bed['inspection_fee'])}}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    
-
-                    <div class="custom-separator"></div>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                        <label  class="ul-form__label"><strong>Supritendent Pharmacist:</strong></label>
-                            <div><img class="w-25" src="{{ Auth::user()->photo ? asset('images/' . Auth::user()->photo) : asset('admin/dist-assets/images/avatar.jpg') }}" alt=""></div>
-                        </div>
-                    </div>
-                    
-                    <div class="form-row">	
-                        <div class="form-group col-md-4">
-                            <label for="inputEmail3" class="ul-form__label"><strong>Supritendent Pharmacist Name:</strong></label>
-                            <div>{{$registration->hospital_pharmacy->pharmacist_name}}</div>
-                        </div>
-
-                        <div class="form-group col-md-4">
-                            <label for="inputEmail3" class="ul-form__label"><strong>Supritendent Pharmacist Email:</strong></label>
-                            <div>{{$registration->hospital_pharmacy->pharmacist_email}}</div>
-                        </div>
-                        
-                        <div class="form-group col-md-4">
-                            <label for="inputEmail3" class="ul-form__label"><strong>Supritendent Pharmacist Phone:</strong></label>
-                            <div>{{$registration->hospital_pharmacy->pharmacist_phone}}</div>
-                        </div>
-
-                        <div class="form-group col-md-4">
-                            <label for="inputEmail3" class="ul-form__label"><strong>Year of Qualification:</strong></label>
-                            <div>{{$registration->hospital_pharmacy->qualification_year}}</div>
-                        </div>
-                        @if($registration->hospital_pharmacy->registration_no)
-                        <div class="form-group col-md-4">
-                            <label for="inputEmail3" class="ul-form__label"><strong>Registration No.:</strong></label>
-                            <div>{{$registration->hospital_pharmacy->registration_no}}</div>
-                        </div>
-                        @endif
-                        @if($registration->hospital_pharmacy->last_year_licence_no)
-                        <div class="form-group col-md-4">
-                            <label for="inputEmail3" class="ul-form__label"><strong>Last Year Annual License No.:</strong></label>
-                            <div>{{$registration->hospital_pharmacy->last_year_licence_no}}</div>
-                        </div>
-                        @endif
-                        <div class="form-group col-md-4">
-                            <label for="inputEmail3" class="ul-form__label"><strong>Residential Address:</strong></label>
-                            <div>{{$registration->hospital_pharmacy->residential_address}}</div>
-                        </div>
-                    </div>
+                <x-hospital-pharmacy-preview
+                :registrationID="$registration->id" 
+                :userID="$registration->user_id" 
+                type="hospital_pharmacy" />
+                
                 <div class="custom-separator"></div>
 
                 <div class="row">
