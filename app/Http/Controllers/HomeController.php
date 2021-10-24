12c25f308b51f41d4365627497bb175981751451
Papai Sarkar <iampapaisarkar@gmail.com>
2021-10-24 20:51:13 +0530

    working on vendor profile dashbaord

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -281,106 +281,20 @@ class HomeController extends Controller
                 'licence_issued' => $licence_issued,
             ];
         }
-        if(Auth::user()->hasRole(['vendor'])){
-            $meptpApplication = MEPTPApplication::where(['vendor_id' => Auth::user()->id])->latest()->first();
-
-            if($meptpApplication){
-                if($meptpApplication->status == 'send_to_state_office'){
-                    $data = [
-                        'status' => 'PENDING',
-                        'type' => 'METPT'
-                    ];
-                }
-                if($meptpApplication->status == 'reject_by_state_office'){
-                    $data = [
-                        'status' => 'DOCS. QUERIED',
-                        'type' => 'METPT'
-                    ];
-                }
-                if($meptpApplication->status == 'send_to_pharmacy_practice'){
-                    $data = [
-                        'status' => 'PENDING',
-                        'type' => 'METPT'
-                    ];
-                }
-                if($meptpApplication->status == 'reject_by_pharmacy_practice'){
-                    $data = [
-                        'status' => 'DOCS. QUERIED',
-                        'type' => 'METPT'
-                    ];
-                }
-                if($meptpApplication->status == 'approved_tier_selected'){
-                    $data = [
-                        'status' => 'APPROVED',
-                        'type' => 'METPT'
-                    ];
-                }
-                if($meptpApplication->status == 'index_generated'){
-                    $data = [
-                        'status' => 'EXAM CARD',
-                        'type' => 'METPT'
-                    ];
-                }
-                if($meptpApplication->status == 'pass'){
-                    $data = [
-                        'status' => 'PASSED',
-                        'type' => 'METPT'
-                    ];
-                }
-                if($meptpApplication->status == 'fail'){
-                    $data = [
-                        'status' => 'FAILED',
-                        'type' => 'METPT'
-                    ];
-                }
-
-                $ppmvRenewal = PPMVRenewal::where(['vendor_id' => Auth::user()->id, 'meptp_application_id' => $meptpApplication->id])->latest()->first();
-
-                if($ppmvRenewal){
-                    if($ppmvRenewal->status == 'send_to_state_office'){
-                        $data = [
-                            'status' => 'PENDING',
-                            'type' => 'Tiered PPMV Registration'
-                        ];
-                    }
-                    if($ppmvRenewal->status == 'rejected'){
-                        $data = [
-                            'status' => 'DOCS. QUERIED',
-                            'type' => 'Tiered PPMV Registration'
-                        ];
-                    }
-                    if($ppmvRenewal->status == 'approved'){
-                        $data = [
-                            'status' => 'APPROVED',
-                            'type' => 'Tiered PPMV Registration'
-                        ];
-                    }
-                    if($ppmvRenewal->status == 'recommended'){
-                        $data = [
-                            'status' => 'RECOMMENDED',
-                            'type' => 'Tiered PPMV Registration'
-                        ];
-                    }
-                    if($ppmvRenewal->status == 'unrecommended'){
-                        $data = [
-                            'status' => 'NOT RECOMMENDED',
-                            'type' => 'Tiered PPMV Registration'
-                        ];
-                    }
-                    if($ppmvRenewal->status == 'licence_issued'){
-                        $data = [
-                            'status' => 'LICECNE ISSUED',
-                            'type' => 'Tiered PPMV Registration'
-                        ];
-                    }
-                }
-            }else{
-                $data = [
-                    'status' => 'NO DATA',
-                    'type' => 'Date not found yet'
-                ];
-            }
-
+        if(Auth::user()->hasRole(['hospital_pharmacy'])){
+            
+        }
+        if(Auth::user()->hasRole(['community_pharmacy'])){
+            
+        }
+        if(Auth::user()->hasRole(['distribution_premises'])){
+            
+        }
+        if(Auth::user()->hasRole(['manufacturing_premises'])){
+            
+        }
+        if(Auth::user()->hasRole(['ppmv'])){
+            
         }
         return view('index', compact('data'));
     }
