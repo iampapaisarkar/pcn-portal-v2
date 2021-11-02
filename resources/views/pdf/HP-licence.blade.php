229d25108e27663df9827e4e4e779ae7538aff5a
iampapaisarkar <iampapaisarkar@gmail.com>
2021-11-02 22:19:34 +0530

    HP pdf licence done

--- resources/views/pdf/HP-licence.blade.php
+++ resources/views/pdf/HP-licence.blade.php
@@ -5,41 +5,53 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>
-            body{
-                font-family: Arial;
-            }
-            @page { 
-                size: 570pt 800pt; 
-                padding: 0;
-                margin: 0;
-            }
-        </style>
     </head>
-    <body>
-        <div style="background-image: url({{$background}}); width: 100%; height: 100%;">
-            <div style="position:absolute;left:0px;top:0px">
-            <img src="971806b0-36de-11ec-a980-0cc47a792c0a_id_971806b0-36de-11ec-a980-0cc47a792c0a_files/background1.jpg" width=595 height=841></div>
-            <div style="position:absolute;left:80.44px;top:28.18px" class="cls_002"><span class="cls_002">PHARMACISTS COUNCIL OF NIGERIA</span></div>
-            <div style="position:absolute;left:413.68px;top:103.71px" class="cls_003"><span class="cls_003">LICENCE NUMBER</span></div>
-            <div style="position:absolute;left:405.59px;top:118.40px" class="cls_004"><span class="cls_004">FCJU000520</span></div>
-            <div style="position:absolute;left:39.92px;top:205.45px" class="cls_005"><span class="cls_005">Certificate of Registration/Retention of</span></div>
-            <div style="position:absolute;left:114.48px;top:243.61px" class="cls_005"><span class="cls_005">Name of Hospital Pharmacy</span></div>
-            <div style="position:absolute;left:137.18px;top:300.39px" class="cls_008"><span class="cls_008">JULIUS BERGER HOSPITAL</span></div>
-            <div style="position:absolute;left:262.22px;top:343.79px" class="cls_009"><span class="cls_009">Situated at:</span></div>
-            <div style="position:absolute;left:235.56px;top:366.88px" class="cls_010"><span class="cls_010">LIFE CAMP, FCT</span></div>
-            <div style="position:absolute;left:215.14px;top:408.58px" class="cls_009"><span class="cls_009">applied to be registered by</span></div>
-            <div style="position:absolute;left:204.15px;top:441.91px" class="cls_010"><span class="cls_010">Pharm. SHABA SAMUEL</span></div>
-            <div style="position:absolute;left:98.02px;top:471.14px" class="cls_009"><span class="cls_009">was on August 7, 2021 duly registered for the purpose of mixing,</span></div>
-            <div style="position:absolute;left:82.83px;top:486.78px" class="cls_009"><span class="cls_009">compounding, preparing, dispensing, procuring of drugs and poisons,</span></div>
-            <div style="position:absolute;left:71.18px;top:502.42px" class="cls_009"><span class="cls_009">counselling and pharmaceutical care in accordance with provisions of the</span></div>
-            <div style="position:absolute;left:120.61px;top:518.06px" class="cls_009"><span class="cls_009">Pharmacists Council of Nigeria Act, Cap P17, LFN, 2004.</span></div>
-            <div style="position:absolute;left:204.83px;top:611.37px" class="cls_007"><span class="cls_007">REGISTRAR / SECRETARY</span></div>
-            <div style="position:absolute;left:211.32px;top:638.10px" class="cls_013"><span class="cls_013">DATED THIS 7TH DAY OF  AUG, 2021</span></div>
-            <div style="position:absolute;left:205.47px;top:649.27px" class="cls_013"><span class="cls_013">LICENCE EXPIRES ON 31ST DEC, 2021</span></div>
-            <div style="position:absolute;left:217.62px;top:738.55px" class="cls_013"><span class="cls_013">FEDERAL REPUBLIC OF NIGERIA</span></div>
-            <div style="position:absolute;left:482.44px;top:807.09px" class="cls_014"><span class="cls_014">NOT-TRANSFERABLE</span></div>
-            <div style="position:absolute;left:293.68px;top:818.26px" class="cls_014"><span class="cls_014">VALID FOR ONLY THE APPROVED LOCATION STATED ABOVE</span></div>
+    <style type="text/css">
+        body{
+                font-family: sans-serif;
+        }
+        @page { 
+            size: 570pt 800pt; 
+            padding: 0;
+            margin: 0;
+        }
+    </style>
+<body style="background-image: url({{$background}}); width: 100%; height: 100%;">
+    <div style="padding-top: 32px;padding-left: 70px;padding-right: 70px;">
+        <div style="text-align: center; font-weight: bold; font-size: 30px;"><span>PHARMACISTS COUNCIL OF NIGERIA</span></div>
+        
+        <div style="position: absolute; top: 100px;">
+            <div style="margin-bottom: 10px; text-align: right;"><span>LICENCE NUMBER</span></div>
+            <div style="text-align: right; font-size: 20px; color: #bb1c25;"><span>{{$data['licence']}}</span></div>
         </div>
-    </body>
+        
+        <div style="position: absolute; top: 250px;">
+            <div style="margin-bottom: 20px; font-size: 35px; text-align: center;  color: #bb1c25;"><span>Certificate of Registration/Retention ofName of Hospital Pharmacy</span></div>
+            <div style="margin-bottom: 20px; font-size: 25px; text-align: center;  color: #bb1c25; font-weight: bold;">{{ucwords($data['user']['hospital_name'])}}</div>
+            
+            <div style="margin-bottom: 20px; font-size: 14px; text-align: center;"><span>Situated at:</span></div>
+            <div style="margin-bottom: 20px; font-size: 18px; text-align: center; color: #bb1c25; font-weight: bold;"><span>{{ucwords($data['hospital_pharmacy']['pharmacist_name'])}}</span></div>
+            
+            <div style="margin-bottom: 20px; font-size: 14px; text-align: center;"><span>applied to be registered by</span></div>
+            <div style="margin-bottom: 20px; font-size: 18px; text-align: center; color: #bb1c25; font-weight: bold;"><span>Pharm. {{ucwords($data['user']['firstname'])}} {{ucwords($data['user']['lastname'])}}</span></div>
+            
+            <div style="font-size: 14px; text-align: center;"><span>was on {{strtoupper($data['updated_at']->format('jS'))}} DAY OF  {{strtoupper($data['updated_at']->format('M'))}}, {{$data['updated_at']->format('Y')}} duly registered for the purpose of mixing,compounding, preparing, dispensing, procuring of drugs and poisons,counselling and pharmaceutical care in accordance with provisions of thePharmacists Council of Nigeria Act, Cap P17, LFN, 2004.</span></div>
+        </div>
+        
+        <div style="position: absolute; top: 760px;">
+            <div style="margin-bottom: 20px; font-size: 16px; text-align: center;"><span>REGISTRAR / SECRETARY</span></div>
+            <div style="font-size: 10px; text-align: center;"><span>DATED THIS {{strtoupper($data['updated_at']->format('jS'))}} DAY OF  {{strtoupper($data['updated_at']->format('M'))}}, {{$data['updated_at']->format('Y')}} LICENCE EXPIRES ON {{strtoupper(\Carbon\Carbon::parse($data['expires_at'])->format('jS'))}} {{strtoupper(\Carbon\Carbon::parse($data['expires_at'])->format('M'))}}, {{\Carbon\Carbon::parse($data['expires_at'])->format('Y')}}</span></div>
+        </div>
+        
+        <div style="position: absolute; top: 920px;">
+            <div style="font-size: 10px; text-align: center;"><span>FEDERAL REPUBLIC OF NIGERIA</span></div>
+        </div>
+       
+        <div style="position: absolute; top: 1000px;">
+            <div style="font-size: 10px; text-align: right; color: #fff;"><span>FEDERAL REPUBLIC OF NIGERIA</span></div>
+            <div style="font-size: 10px; text-align: right; color: #fff;"><span>VALID FOR ONLY THE APPROVED LOCATION STATED ABOVE</span></div>
+        </div>
+    </div>
+
+</body>
 </html>
