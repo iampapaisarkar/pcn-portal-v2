b9cb6ef413f50b01b5c0a2224166717ca5970a1a
iampapaisarkar <iampapaisarkar@gmail.com>
2021-11-12 16:17:20 +0530

    renew HP form issue fix

--- app/Http/Controllers/HospitalPharmacy/RenewalController.php
+++ app/Http/Controllers/HospitalPharmacy/RenewalController.php
@@ -127,7 +127,7 @@ class RenewalController extends Controller
 
         $isRenewal = Renewal::where(['user_id' => Auth::user()->id, 'type' => 'hospital_pharmacy_renewal'])
         ->latest()->first();
-        if($isRenewal && ($isRenewal->status != 'send_to_registry' && $isRenewal->status != 'send_to_registration' && $isRenewal->status != 'no_recommendation')){
+        if($isRenewal && ($isRenewal->status == 'send_to_registry' && $isRenewal->status == 'send_to_registration' && $isRenewal->status == 'no_recommendation')){
             return redirect()->route('hospital-renew');
         }
 
