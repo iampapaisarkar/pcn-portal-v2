97065fd965fa2299c645e0a134f81ea3be4861c7
iampapaisarkar <iampapaisarkar@gmail.com>
2021-11-13 14:35:08 +0530

    all issues fix

--- config/custom/renewal.php
+++ config/custom/renewal.php
@@ -1,11 +1,11 @@
 <?php
-return 
-    [
-        'renewal_year' => \Carbon\Carbon::now()->addYears(1)->format('Y'),
-        'expires_at' => \Carbon\Carbon::now()->addDays(1)->format('Y-m-d'),
-        'check_renewal_date' => \Carbon\Carbon::createFromFormat('Y-m-d', $renwal->expires_at)->format('Y-m-d'),
-        'renewal_date' => \Carbon\Carbon::createFromFormat('Y-m-d', $renwal->expires_at)->format('d M, Y')
-    ];
+// return 
+    // [
+    //     'renewal_year' => \Carbon\Carbon::now()->addYears(1)->format('Y'),
+    //     'expires_at' => \Carbon\Carbon::now()->addDays(1)->format('Y-m-d'),
+    //     'check_renewal_date' => \Carbon\Carbon::createFromFormat('Y-m-d', $renwal->expires_at)->format('Y-m-d'),
+    //     'renewal_date' => \Carbon\Carbon::createFromFormat('Y-m-d', $renwal->expires_at)->format('d M, Y')
+    // ];
 
 // return 
     // [
