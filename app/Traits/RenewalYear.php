c54e83b8c32e0b87b0a9c8fc016a26b1d5074e72
iampapaisarkar <iampapaisarkar@gmail.com>
2021-11-13 14:13:58 +0530

    working on renewal dynamic date

--- app/Traits/RenewalYear.php
+++ app/Traits/RenewalYear.php
@@ -5,16 +5,16 @@ namespace App\Traits;
 trait RenewalYear{
   
     public function renewal_year() {
-        return \Carbon\Carbon::now()->addYears(1)->format('Y');
+        return date('Y-m-d');
     }
     public function expires_at() {
-        return \Carbon\Carbon::now()->addDays(1)->format('Y-m-d');
+        return date('Y-m-d');
     }
     public function check_renewal_date($expires_at) {
-        return \Carbon\Carbon::createFromFormat('Y-m-d', $expires_at)->format('Y-m-d');
+        return date('Y-m-d');
     }
     public function renewal_date($expires_at) {
-        return \Carbon\Carbon::createFromFormat('Y-m-d', $expires_at)->format('d M, Y');
+        return date('Y-m-d');
     }
 
 
