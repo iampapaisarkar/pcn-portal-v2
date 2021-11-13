6160424ba4644f48eb9b1c3dad290360bd5760ef
iampapaisarkar <iampapaisarkar@gmail.com>
2021-11-13 14:38:20 +0530

    renewal date issue working

--- app/Http/Services/RenewalDates.php
+++ app/Http/Services/RenewalDates.php
@@ -6,18 +6,18 @@ use DB;
 
 class RenewalDates{
   
-    // public static function renewal_year() {
-    //     return Carbon::now()->addYears(1)->format('Y');
-    // }
-    // public static function expires_at() {
-    //     return Carbon::now()->addDays(1)->format('Y-m-d');
-    // }
-    // public static function check_renewal_date($expires_at) {
-    //     return Carbon::createFromFormat('Y-m-d', $expires_at)->format('Y-m-d');
-    // }
-    // public static function renewal_date($expires_at) {
-    //     return Carbon::createFromFormat('Y-m-d', $expires_at)->format('d M, Y');
-    // }
+    public static function renewal_year() {
+        return Carbon::now()->addYears(1)->format('Y');
+    }
+    public static function expires_at() {
+        return Carbon::now()->addDays(1)->format('Y-m-d');
+    }
+    public static function check_renewal_date($expires_at) {
+        return Carbon::createFromFormat('Y-m-d', $expires_at)->format('Y-m-d');
+    }
+    public static function renewal_date($expires_at) {
+        return Carbon::createFromFormat('Y-m-d', $expires_at)->format('d M, Y');
+    }
 
 
     // public static function renewal_year() {
