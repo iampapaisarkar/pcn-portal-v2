94c713fce06d1220b2cf15d8bf7f0133327ee661
iampapaisarkar <iampapaisarkar@gmail.com>
2021-11-24 21:40:52 +0530

    working on licecne

--- resources/views/pdf/HP-licence.blade.php
+++ resources/views/pdf/HP-licence.blade.php
@@ -23,14 +23,14 @@
         }
     </style>
 <body style="background-image: url({{$background}}); width: 100%; height: 100%;">
-    <div style="padding-top: 32px;padding-left: 70px;padding-right: 70px;">
+    <div style="padding-top: 32px;padding-left: 70px;padding-right: 70px; width: 100%;">
         <div style="text-align: center; font-weight: bold; font-size: 30px;"><span>PHARMACISTS COUNCIL OF NIGERIA</span></div>
         
         <div style="position: absolute; top: 75px; left: 45px;">
             <img src="{{$photo}}" style="width: 170px; height: 170px; border-radius: 20px;" />
         </div>
         
-        <div style="position: absolute; top: 100px; text-align: right;">
+        <div style="position: absolute; top: 100px;">
             <div style="margin-bottom: 10px; text-align: right;"><span>LICENCE NUMBER</span></div>
             <div style="text-align: right; font-size: 20px; color: #bb1c25;"><span>{{$data['licence']}}</span></div>
         </div>
@@ -48,16 +48,16 @@
             <div style="font-size: 14px; text-align: center;"><span>was on <strong>{{strtoupper($data['updated_at']->format('jS'))}} DAY OF  {{strtoupper($data['updated_at']->format('M'))}}, {{$data['updated_at']->format('Y')}}</strong> duly registered for the purpose of mixing,compounding, preparing, dispensing, procuring of drugs and poisons,counselling and pharmaceutical care in accordance with provisions of thePharmacists Council of Nigeria Act, Cap P17, LFN, 2004.</span></div>
         </div>
         
-        <div style="position: absolute; top: 760px; text-align: right;">
+        <div style="position: absolute; top: 760px;">
             <div style="margin-bottom: 20px; font-size: 16px; text-align: center;"><span>REGISTRAR / SECRETARY</span></div>
             <div style="font-size: 10px; text-align: center;"><span>DATED THIS {{strtoupper($data['updated_at']->format('jS'))}} DAY OF  {{strtoupper($data['updated_at']->format('M'))}}, {{$data['updated_at']->format('Y')}} LICENCE EXPIRES ON {{strtoupper(\Carbon\Carbon::parse($data['expires_at'])->format('jS'))}} {{strtoupper(\Carbon\Carbon::parse($data['expires_at'])->format('M'))}}, {{\Carbon\Carbon::parse($data['expires_at'])->format('Y')}}</span></div>
         </div>
         
-        <div style="position: absolute; top: 920px; text-align: right;">
+        <div style="position: absolute; top: 920px;">
             <div style="font-size: 10px; text-align: center;"><span>FEDERAL REPUBLIC OF NIGERIA</span></div>
         </div>
        
-        <div style="position: absolute; top: 1000px; text-align: right;">
+        <div style="position: absolute; top: 1000px;">
             <div style="font-size: 10px; text-align: right; color: #fff;"><span>FEDERAL REPUBLIC OF NIGERIA</span></div>
             <div style="font-size: 10px; text-align: right; color: #fff;"><span>VALID FOR ONLY THE APPROVED LOCATION STATED ABOVE</span></div>
         </div>
