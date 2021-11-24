9821e32df67364fe35eeb1ae6d69be20b9841da9
iampapaisarkar <iampapaisarkar@gmail.com>
2021-11-24 22:01:43 +0530

    hp licence issue fix

--- resources/views/pdf/CP-DP-MP-licence.blade.php
+++ resources/views/pdf/CP-DP-MP-licence.blade.php
@@ -30,12 +30,12 @@
             <img src="{{$photo}}" style="width: 170px; height: 170px; border-radius: 20px;" />
         </div>
 
-        <div style="position: absolute; top: 100px;">
+        <div style="margin-top: 50px;">
             <div style="margin-bottom: 10px; text-align: right;"><span>LICENCE NUMBER</span></div>
             <div style="text-align: right; font-size: 20px; color: #bb1c25;"><span>{{$data['licence']}}</span></div>
         </div>
         
-        <div style="position: absolute; top: 240px;">
+        <div style="margin-top: 100px;">
             @if($type == 'community_pharmacy_renewal')
             <div style="font-family: 'OLDENGL'; margin-bottom: 20px; font-size: 38px; text-align: center;  color: #bb1c25;"><span>Certificate of Registration/Retention of Community Pharmacy Premises</span></div>
             @endif
@@ -66,16 +66,16 @@
             @endif
         </div>
         
-        <div style="position: absolute; top: 760px;">
+        <div style="margin-top: 110px;">
             <div style="margin-bottom: 20px; font-size: 16px; text-align: center;"><span>REGISTRAR / SECRETARY</span></div>
             <div style="font-size: 10px; text-align: center;"><span>DATED THIS {{strtoupper($data['updated_at']->format('jS'))}} DAY OF  {{strtoupper($data['updated_at']->format('M'))}}, {{$data['updated_at']->format('Y')}} LICENCE EXPIRES ON {{strtoupper(\Carbon\Carbon::parse($data['expires_at'])->format('jS'))}} {{strtoupper(\Carbon\Carbon::parse($data['expires_at'])->format('M'))}}, {{\Carbon\Carbon::parse($data['expires_at'])->format('Y')}}</span></div>
         </div>
         
-        <div style="position: absolute; top: 920px;">
+        <div style="margin-top: 105px;">
             <div style="font-size: 10px; text-align: center;"><span>FEDERAL REPUBLIC OF NIGERIA</span></div>
         </div>
        
-        <div style="position: absolute; top: 1000px;">
+        <div style="margin-top: 60px;">
             <div style="font-size: 10px; text-align: right; color: #fff;"><span>FEDERAL REPUBLIC OF NIGERIA</span></div>
             <div style="font-size: 10px; text-align: right; color: #fff;"><span>VALID FOR ONLY THE APPROVED LOCATION STATED ABOVE</span></div>
         </div>
